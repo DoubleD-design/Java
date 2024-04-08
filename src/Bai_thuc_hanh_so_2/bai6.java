37df8e3fcac356142c95698765fa5925855d40db
TMacbookT CENTER <macbook@Macbooks-MacBook-Pro.local>
2024-04-08 12:40:48 +0700

    fix bai6

--- src/Bai_thuc_hanh_so_2/bai6.java
+++ src/Bai_thuc_hanh_so_2/bai6.java
@@ -1,52 +1,92 @@
 package Bai_thuc_hanh_so_2;
-import java.awt.*;
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
 import java.awt.event.*;
-public class bai6 extends JFrame implements ActionListener{
-    JButton btnOk;
-    JTextField txt,txt1;
-    List ls;
 
-    JPanel pn;
-    public void GUI(){
-        btnOk = new JButton("OK");
-        txt = new JTextField(20);
-        ls = new List(3);
-        ls.add("Tiger");
-        ls.add("Lion");
-        ls.add("Elephant");
-        pn = new JPanel(new FlowLayout());
-        pn.add(btnOk);
-        pn.add(txt);
-        pn.add(ls);
-        txt1 = new JTextField("The event is displayed here");
-        txt1.setEditable(false);
-        pn.add(txt1);
-        add(pn);
-        btnOk.addActionListener(this);
-        txt.addActionListener(this);
-        ls.addActionListener(this);
-        setSize(510,350);
-        show();
+public class bai6 extends Frame implements ActionListener {
+    JFrame mainFrame;
+    JLabel lb_display;
+    JPanel pn, pn1, pn2;
+    JButton butt_ok;
+    JTextField txtstring;
+    List animallist;
+
+    public void GUI() {
+        mainFrame = new JFrame("Action Event Test");
+        mainFrame.setSize(400, 250);
+        mainFrame.setLayout(new GridLayout(2, 1));
+
+        butt_ok = new JButton("OK");
+        butt_ok.addActionListener(this);
+        txtstring = new JTextField(10);
+        txtstring.getDocument().addDocumentListener(new DocumentListener() {
+
+            @Override//from  w  w  w .  j  av  a 2s.  com
+            public void insertUpdate(DocumentEvent e) {
+                lb_display.setText("Text changed");
+            }
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                lb_display.setText("Text changed");
+
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                lb_display.setText("Text changed");
+
+            }
+
+        });
+        animallist = new List();
+        animallist.add("Tiger");
+        animallist.add("Lion");
+        animallist.add("Elephant");
+        JScrollPane sb = new JScrollPane(animallist,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+//        sb.add(animallist);
+        JScrollBar bar = sb.getVerticalScrollBar();
+        bar.setPreferredSize(new Dimension(10, 0));
+
+        animallist.addActionListener(this);
+        pn1 = new JPanel(new FlowLayout());
+        pn1.add(butt_ok);
+        pn1.add(txtstring);
+        pn1.add(sb);
+
+        lb_display = new JLabel("The event is displayed here");
+        lb_display.setHorizontalAlignment(JLabel.CENTER);
+        lb_display.setVerticalAlignment(JLabel.TOP);
+
+        mainFrame.add(pn1);
+        mainFrame.add(lb_display);
+        mainFrame.setVisible(true);
+
     }
 
-    @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == btnOk){
-            txt1.setText("You have clicked on the button");
+        if (e.getSource() == butt_ok) {
+            lb_display.setText("Button is clicked ");
         }
-        if(e.getSource() == txt){
-            txt1.setText("You have typed " + txt.getText());
+        if (e.getSource() == txtstring) {
+            lb_display.setText("TextField is written: " + txtstring.getText());
         }
-        if(e.getSource() == ls){
-            txt1.setText("You have selected "+ls.getSelectedItem());
+        if (e.getSource() == animallist) {
+            lb_display.setText("You double click on the" + e.getActionCommand());
         }
     }
-    public bai6(String title){
-        super(title);
+
+    public bai6() {
         GUI();
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                dispose();
+            }
+        });
     }
-    public  static void main(String[] args){
-        new bai6("Action Event");
+
+    public static void main(String[] args) {
+        new bai6();
     }
 }
